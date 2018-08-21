daf3ce8b942f23da570eb1c7a4da1d808dfabe54
clkent <chelsea.kent@gmail.com>
2018-08-21 13:43:37 -0700

    Bonus credit for functions as argument 2

--- drills.js
+++ drills.js
@@ -29,16 +29,19 @@ function filter(array, fn) {
 // First we setup an array of strings we plan to filter:
 const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-// We use your `filter` function here, capturing a new array into `filteredNames`
-// comprised of names that only begin with 'R'
-const filteredNames = filter(myNames, function(name) {
-    // This is known as a "predicate function" - it's a function that 
-    // only returns a boolean
-    return name[0] === 'R';
-});
+// // We use your `filter` function here, capturing a new array into `filteredNames`
+// // comprised of names that only begin with 'R'
+// const filteredNames = filter(myNames, function(name) {
+//     // This is known as a "predicate function" - it's a function that 
+//     // only returns a boolean
+//     return name[0] === 'R';
+// });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
-// <---- DO NOT EDIT BETWEEN THESE LINES
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// // // <---- DO NOT EDIT BETWEEN THESE LINES
+
+// bonus credit
+console.log(filter(myNames, name => name[0] === 'R'));
 
 
 //---------------------------------------------------------------------
